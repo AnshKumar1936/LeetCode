da1449b626fe1a358a854055edc296a0ee42d8ba
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-08-23 18:55:19 +0530

    Update Fraction Addition and Subtraction.java

--- Medium/Fraction Addition and Subtraction.java
+++ Medium/Fraction Addition and Subtraction.java
@@ -1,31 +1,44 @@
 class Solution {
     public String fractionAddition(String expression) {
         int numerator = 0, denominator = 1;
-        
-        Pattern pattern = Pattern.compile("([+-]?\\d+)/(\\d+)");
-        Matcher matcher = pattern.matcher(expression);
-        
-        while (matcher.find()) {
-            int num = Integer.parseInt(matcher.group(1));
-            int den = Integer.parseInt(matcher.group(2));
+        int i = 0, n = expression.length();
+
+        while (i < n) {
+            int sign = 1;
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+                sign = expression.charAt(i) == '-' ? -1 : 1;
+                i++;
+            }
+
+            int num = 0;
+            while (i < n && Character.isDigit(expression.charAt(i))) {
+                num = num * 10 + (expression.charAt(i) - '0');
+                i++;
+            }
+
+            num *= sign;
+
+            i++; // Skip the '/'
             
-            numerator = numerator * den + num * denominator;
-            denominator *= den;
-            
-            int gcdVal = gcd(Math.abs(numerator), denominator);
-            numerator /= gcdVal;
-            denominator /= gcdVal;
+            int denom = 0;
+            while (i < n && Character.isDigit(expression.charAt(i))) {
+                denom = denom * 10 + (expression.charAt(i) - '0');
+                i++;
+            }
+
+            numerator = numerator * denom + num * denominator;
+            denominator *= denom;
+
+            int gcd = gcd(Math.abs(numerator), denominator);
+            numerator /= gcd;
+            denominator /= gcd;
         }
-        
+
         return numerator + "/" + denominator;
     }
-    
+
     private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        return b == 0 ? a : gcd(b, a % b);
     }
+
 }
